29e31c36c064ba9a21b8372d37b36c4f38697ce3
Lorenzo Fernandez <lorenzo.kniss@gmail.com>
2019-01-29 16:46:25 -0200

    Created a footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted">© 2018 Copyright <a :href="link"> {{ text }} </a> </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: "Footer",
+  props: [
+      'link',
+      'text'
+  ]
+}
+
+</script>
+
+<style scoped>
+  
+  .footer {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 60px;
+      line-height: 60px;
+      background-color: #007bff;
+  }
+
+  a, .text-muted {
+    color: #fff !important;
+  }
+
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,37 +1,39 @@
 <template>
-  <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
-    <router-link :to="'/'" class="navbar-brand"> {{ brand }} </router-link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li v-for="routes in links" class="nav-item" :class="routes.children ? 'dropdown' : ''">
-            
-            <div v-if="routes.children">
-                <a class="nav-link dropdown-toggle" href="#" v-bind:id="routes.id" data-toggle="dropdown">
+  <header>
+    <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
+      <router-link :to="'/'" class="navbar-brand"> {{ brand }} </router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li v-for="routes in links" class="nav-item" :class="routes.children ? 'dropdown' : ''">
+              
+              <div v-if="routes.children">
+                  <a class="nav-link dropdown-toggle" href="#" v-bind:id="routes.id" data-toggle="dropdown">
+                    {{ routes.text }}
+                  </a>
+                  <div class="dropdown-menu">
+                    <router-link 
+                        v-for="children in routes.children" 
+                        :key="children.id"
+                        :to="`${children.path}`"
+                        class="dropdown-item"> 
+                      {{ children.text }}
+                    </router-link>
+                  </div>
+              </div>
+              <div v-else>
+                <router-link :key="routes.id" :to="`${routes.path}`" class="nav-link"> 
                   {{ routes.text }}
-                </a>
-                <div class="dropdown-menu">
-                  <router-link 
-                      v-for="children in routes.children" 
-                      :key="children.id"
-                      :to="`${children.path}`"
-                      class="dropdown-item"> 
-                    {{ children.text }}
-                  </router-link>
-                </div>
-            </div>
-            <div v-else>
-              <router-link :key="routes.id" :to="`${routes.path}`" class="nav-link"> 
-                {{ routes.text }}
-              </router-link>  
-            </div>
+                </router-link>  
+              </div>
 
-        </li>
-      </ul>
-    </div>
-  </nav>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
